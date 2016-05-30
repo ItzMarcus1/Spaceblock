a3a45c1832eabcbcf85f0b7d50e5ab07be482dc2
DarkStarPvP <admin@darkstarpvp.com>
2016-05-30 16:24:44 +0200

    Added packages

--- src/net/spaceblock/Core.java
+++ src/net/spaceblock/Core.java
@@ -1,14 +1,22 @@
 package net.spaceblock;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Created by marcus on 30-05-2016.
- */
 public class Core extends JavaPlugin implements Listener {
 
+    public void newEvent(Listener listener) {
+        Bukkit.getPluginManager().registerEvents(listener, this);
+    }
+
+    public void newCommand(String command, CommandExecutor commandExecutor) {
+        getCommand(command).setExecutor(commandExecutor);
+    }
+
     public void onEnable() {
-        
+
     }
 }
