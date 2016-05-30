50a097ed67f1731f7e4179b20fff805d4f74e0da
DarkStarPvP <admin@darkstarpvp.com>
2016-05-30 16:19:59 +0200

    First commit

--- src/net/spaceblock/Core.java
+++ src/net/spaceblock/Core.java
@@ -1,7 +1,14 @@
 package net.spaceblock;
 
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by marcus on 30-05-2016.
  */
-public class Core {
+public class Core extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        
+    }
 }
