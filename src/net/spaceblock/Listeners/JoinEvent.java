723b2622d64a04ef216d2f21da84220ef1fa0ded
DarkStarPvP <admin@darkstarpvp.com>
2016-05-30 16:35:28 +0200

    Added stuff

---
+++ src/net/spaceblock/Listeners/JoinEvent.java
@@ -0,0 +1,7 @@
+package net.spaceblock.Listeners;
+
+/**
+ * Created by marcus on 30-05-2016.
+ */
+public class JoinEvent {
+}
---
+++ src/net/spaceblock/MySQL/ConnectionHandler.java
@@ -0,0 +1,6 @@
+package net.spaceblock.MySQL;
+
+public class ConnectionHandler {
+
+
+}
